70d02a4803f9eac4b6aeea61f02ae7a42e847ca7
Tivalsdeveloper <tivalsdeveloper@gmail.com>
2026-08-30 12:23:11 +0200

    Add src/tivelweb/__init__.py

---
+++ src/tivelweb/__init__.py
@@ -0,0 +1,8 @@
+"""TivelWeb public API."""
+
+from .components import button, card, hero, image, section
+from .site import Page, Site
+
+__all__ = ["Site", "Page", "hero", "section", "card", "button", "image"]
+__version__ = "0.1.0"
+
