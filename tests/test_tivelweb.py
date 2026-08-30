6cb63ca3e7cac630538810ffa6025bfa09e2d6be
Tivalsdeveloper <tivalsdeveloper@gmail.com>
2026-08-30 12:49:06 +0200

    Test news website template

--- tests/test_tivelweb.py
+++ tests/test_tivelweb.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from tempfile import TemporaryDirectory
 import unittest
 
-from tivelweb import Site, hero
+from tivelweb import Site, create_news_site, hero
 
 
 class SiteTests(unittest.TestCase):
@@ -15,7 +15,13 @@ class SiteTests(unittest.TestCase):
             self.assertIn("Hello", html)
             self.assertIn("Powered by tivalsdeveloper", html)
 
+    def test_news_template(self):
+        with TemporaryDirectory() as folder:
+            site = create_news_site(output=folder)
+            self.assertTrue((Path(folder) / "index.html").exists())
+            self.assertTrue((Path(folder) / "technology.html").exists())
+            self.assertEqual(len(site.pages), 7)
+
 
 if __name__ == "__main__":
     unittest.main()
-
