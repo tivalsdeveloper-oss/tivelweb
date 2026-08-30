f63c0efa2b5598a9fdc44f909b1e0f9ba0c04af3
Tivalsdeveloper <tivalsdeveloper@gmail.com>
2026-08-30 13:02:00 +0200

    Export templates and styles

--- src/tivelweb/__init__.py
+++ src/tivelweb/__init__.py
@@ -2,7 +2,12 @@
 
 from .components import button, card, hero, image, section
 from .site import Page, Site
-from .templates import create_news_site
+from .styles import STYLE_PRESETS, style_preset, theme_color
+from .templates import create_blog_site, create_business_site, create_news_site, create_portfolio_site
 
-__all__ = ["Site", "Page", "hero", "section", "card", "button", "image", "create_news_site"]
-__version__ = "0.2.0"
+__all__ = [
+    "Site", "Page", "hero", "section", "card", "button", "image",
+    "create_news_site", "create_portfolio_site", "create_business_site", "create_blog_site",
+    "STYLE_PRESETS", "style_preset", "theme_color",
+]
+__version__ = "0.3.0"
