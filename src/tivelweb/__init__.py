93e26c6513552791b58e86f3d254a47dba0cd733
Tivalsdeveloper <tivalsdeveloper@gmail.com>
2026-08-30 12:49:05 +0200

    Export news website template

--- src/tivelweb/__init__.py
+++ src/tivelweb/__init__.py
@@ -2,7 +2,7 @@
 
 from .components import button, card, hero, image, section
 from .site import Page, Site
+from .templates import create_news_site
 
-__all__ = ["Site", "Page", "hero", "section", "card", "button", "image"]
-__version__ = "0.1.0"
-
+__all__ = ["Site", "Page", "hero", "section", "card", "button", "image", "create_news_site"]
+__version__ = "0.2.0"
